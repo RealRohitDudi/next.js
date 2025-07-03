5e40e34e58259202dcc5f80b94cd1440ac998291
Rohit Dudi <rohitdudijat@yahoo.com>
2025-07-03 09:52:24 +0530

    Setup next for next project

---
+++ project-001/src/app/signup/layout.tsx
@@ -0,0 +1,10 @@
+export default function signupLayout() {
+  return (
+<html lang="en">
+      <body
+      >
+        <h2>Layout for signup page</h2>
+        
+      </body>
+    </html>    );
+}
---
+++ project-001/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-2xl text-center text-green-500'>Welcome to signup page route</div>
+  )
+}
+
+export default page
